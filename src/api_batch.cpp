226bce5867907cf26638dd20c6c71c37aff8637a
Alex Jeffrey Riniker <alexjriniker@gmail.com>
2023-10-24 18:15:49 -0500

    changed ApiBatch constructor behavior

--- src/api_batch.cpp
+++ src/api_batch.cpp
@@ -4,17 +4,16 @@ ApiBatch::~ApiBatch() { delete[] items; }
 
 ApiBatch::ApiBatch(unsigned int size, const ApiBatchCallback callback,
                    ApiBatchPrepCallback prep_cb)
-    : size{size}, callback{callback}, prep_cb{prep_cb} {
+    : size{size}, callback{callback}, prep_cb{prep_cb},
+      items{new ApiBatchItem[size]} {
   // this->size = size;
   // this->callback = callback;
   // this->prep_cb = prep_cb;
-  // TODO: move into the rest of the member initializations
-  items = new ApiBatchItem[size];
 }
 
 void ApiBatch::submit() {
   auto payload = prep_cb(items, size);
-  callback(payload.c_str());
+  callback(payload.c_strFi());
 
   index = 0; // rest index back to original position
 }
